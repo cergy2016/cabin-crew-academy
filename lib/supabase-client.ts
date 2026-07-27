3fd95d85a636a1cb6a45d672ca98dfcc553f90d2
hechache <114198650+hechache@users.noreply.github.com>
2026-07-27 23:17:19 +0200

    Correction URL Supabase

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://muqkevgspfbockjftvszt.supabase.co';
+const supabaseUrl = 'https://muqkevgspfbocjftvszt.supabase.co';
 const supabaseAnonKey = 'sb_publishable_g2Lvs2TKpDearxjOUCGcqw_zfmpb1r_';
 
-export const supabase = createClient(supabaseUrl
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
